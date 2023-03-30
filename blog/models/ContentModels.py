5e7233c39cf47e38728b959279e277513fcf0871
mehedi23 <satusatusatu0@gmail.com>
2023-03-30 22:51:47 +0600

    add blog user id

--- blog/models/ContentModels.py
+++ blog/models/ContentModels.py
@@ -1,7 +1,9 @@
 from django.db import models
 from .Categorice import Categorice 
+from account.models import User
 
 class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     banner = models.ImageField(upload_to='images/')
     tittle = models.CharField(max_length=200, default="")
     description = models.TextField(max_length=1000, default="")
