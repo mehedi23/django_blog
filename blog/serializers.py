1c4be2c980236b197971a4fcf6b5ae4ce948b448
mehedi23 <satusatusatu0@gmail.com>
2023-04-11 22:50:04 +0600

    category add and pagination

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,12 +7,16 @@ from account.serializers import UserProfileSerializer
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Categorice
-        fields = ('id', 'name')
+        fields = '__all__'
 
 class BlogSerializer(serializers.ModelSerializer):
     user = UserProfileSerializer(read_only=True)
-    category = CategorySerializer(many=True, read_only=True)
+    category = serializers.SerializerMethodField()
     
     class Meta:
         model = Blog
         fields = ('id', 'user', 'banner', 'tittle', 'description', 'category', 'create_at'  )
+
+    def get_category(self, obj):
+        categories = obj.category.all()
+        return [{'id': category.id, 'name': category.name} for category in categories]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, pagination
 from rest_framework.views import APIView
 from .serializers import BlogSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -21,12 +21,22 @@ class BlogDetailsView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
 
-class AllBlogView(APIView):
-    def get(self, request):
-        blog = Blog.objects.all()
-        serializer = BlogSerializer(blog, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class AllBlogPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_query_param = 'page_number'
+
+
+class AllBlogView(APIView):
+    pagination_class = AllBlogPagination
+
+    def get(self, request):
+        paginator = self.pagination_class()
+        blog = Blog.objects.all()
+        paginated_blog = paginator.paginate_queryset(blog, request)
+        serializer = BlogSerializer(paginated_blog, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
     
 
 
@@ -40,19 +50,21 @@ class UserBlogList(APIView):
     
     def post(self, request):
         serializer = BlogSerializer(data=request.data)
-        category_obj = request.data.get('category')
-
-        if category_obj is None:
-            category_error = {
-                                "category": [
-                                    "This list may not be empty."
-                                ]
-                            }
-            return Response(category_error, status=status.HTTP_400_BAD_REQUEST)
 
         if serializer.is_valid(raise_exception=True):
-            blog = serializer.save(user_id=self.request.user.id, category=request.data['category'])
+            blog = serializer.save(user_id=self.request.user.id)
+
+            category_ids = request.data.get('category', [])
+            
+            for category_id in category_ids:
+                try:
+                    category = Categorice.objects.get(id=category_id)
+                except:
+                    return Response("no category", status=status.HTTP_400_BAD_REQUEST)
+                
+                blog.category.add(category)
             serializer = BlogSerializer(instance=blog)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
